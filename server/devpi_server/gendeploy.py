74d060dca507877558ede2d13426a9243ad6ea30
holger krekel <holger@merlinux.eu>
2013-08-06 15:30:59 +0200

    pin >=virtualenv-1.10 for gendeploy target and use "pip install --pre ..." when devpi-server --gendeploy is operating.

--- server/devpi_server/gendeploy.py
+++ server/devpi_server/gendeploy.py
@@ -136,7 +136,7 @@ def gendeploy(config):
     pip = py.path.local.sysfind("pip", paths=[target.join("bin")])
     tw.line("installing devpi-server and supervisor", bold=True)
     version = devpi_server.__version__
-    subproc(tw, [pip, "install", "supervisor", "devpi-server>=%s" % version])
+    subproc(tw, [pip, "install", "--pre", "supervisor", "devpi-server>=%s" % version])
     tw.line("generating configuration")
     gendeploycfg(config, target, tw=tw)
 
