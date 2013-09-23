7dbc0668016723330634e47f162e39660495c374
holger krekel <holger@merlinux.eu>
2013-09-23 14:29:24 +0200

    bump versions

--- client/devpi/__init__.py
+++ client/devpi/__init__.py
@@ -2,7 +2,7 @@ import sys
 import py
 import logging
 
-__version__ = '1.1.dev5'
+__version__ = '1.1.dev6'
 
 log = logging.getLogger(__name__)
 
--- server/devpi_server/__init__.py
+++ server/devpi_server/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '1.1.dev5'
+__version__ = '1.1.dev6'
