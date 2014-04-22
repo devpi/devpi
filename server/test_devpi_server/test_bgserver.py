64e8b014e9238c202c216e7d787ffa8275e71d15
Florian Schulze <florian.schulze@gmx.net>
2014-04-22 11:52:17 +0200

    Mark test as slow, since it accesses the internet.

--- server/test_devpi_server/test_bgserver.py
+++ server/test_devpi_server/test_bgserver.py
@@ -1,6 +1,8 @@
-
 from devpi_server.main import main
+import pytest
 
+
+@pytest.mark.skipif("not config.option.slow")
 def test_server_commands(tmpdir, monkeypatch):
     monkeypatch.setenv("DEVPI_SERVERDIR", tmpdir)
     main(["devpi-server", "--start", "--port=3499"])
