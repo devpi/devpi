932812edb19bde9641d41cb6e7e8b12c2d173835
Florian Schulze <florian.schulze@gmx.net>
2014-05-08 21:23:08 +0200

    Bump devpi-client version.

--- client/devpi/__init__.py
+++ client/devpi/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = '1.2.2'
+__version__ = '1.3.dev1'
 
 log = logging.getLogger(__name__)
 
--- client/setup.py
+++ client/setup.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
       description="devpi upload/install/... workflow commands for Python "
                   "developers",
       long_description=open("README.rst").read(),
-      version='1.2.2',
+      version='1.3.dev1',
       packages=find_packages(),
       install_requires=install_requires,
       url="https://bitbucket.org/hpk42/devpi",
