78bb1efddb36b010c66caacef040d9fc830b371c
Florian Schulze <florian.schulze@gmx.net>
2014-05-28 11:13:07 +0200

    Use dummy data and make sure the root/pypi indexing code is covered.

--- web/tests/test_main.py
+++ web/tests/test_main.py
@@ -21,15 +21,38 @@ def test_index_created_on_first_run(monkeypatch, tmpdir):
     assert iter_projects.called
 
 
-@pytest.mark.skipif("config.option.fast")
 def test_index_projects_arg(monkeypatch, tmpdir):
+    from devpi_web.main import get_indexer
     import devpi_server.main
-    monkeypatch.setattr(devpi_server.extpypi.PyPIStage, "init_pypi_mirror",
-                        lambda self, proxy: None)
-    monkeypatch.setattr(devpi_server.extpypi.PyPIStage, "name2serials", {},
-                        raising=False)
+    XOM = devpi_server.main.XOM
+    xom_container = []
+
+    def MyXOM(config):
+        xom = XOM(config)
+        xom_container.append(xom)
+        return xom
+    # provide dummy data to avoid fetching mirror data from pypi
+    n2s = {u'foo': 1}
+    n2n = {u'foo': u'foo'}
+    monkeypatch.setattr(
+        devpi_server.extpypi.PyPIStage, "init_pypi_mirror", lambda s, p: None)
+    monkeypatch.setattr(
+        devpi_server.extpypi.PyPIStage, "name2serials", n2s, raising=False)
+    monkeypatch.setattr(
+        devpi_server.extpypi.PyPIStage, "normname2name", n2n, raising=False)
+    monkeypatch.setattr(devpi_server.main, "XOM", MyXOM)
     # if the webserver is started, we fail
     monkeypatch.setattr(devpi_server.main, "wsgi_run", lambda xom: 0 / 0)
     devpi_server.main.main(
         ["devpi-server", "--serverdir", str(tmpdir), "--index-projects"])
     assert tmpdir.join('.indices').check()
+    (xom,) = xom_container
+    ix = get_indexer(xom.config)
+    result = ix.query_projects('foo')
+    assert result['info']['found'] == 1
+    assert result['items'][0]['data'] == {
+        u'index': u'pypi',
+        u'name': u'foo',
+        u'path': u'/root/pypi/foo',
+        u'text_type': u'project',
+        u'user': u'root'}
