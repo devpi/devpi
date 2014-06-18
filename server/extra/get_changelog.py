163a9bad4a157d190be5ea2dd3dddd6822044584
holger krekel <holger@merlinux.eu>
2014-06-18 13:20:21 +0200

    RELEASE-TODO is moved to trello, also pprint changelog script output

--- server/extra/get_changelog.py
+++ server/extra/get_changelog.py
@@ -1,11 +1,12 @@
 import sys
 import py
 from devpi_server.keyfs import KeyFS
+import pprint
 
 if __name__ == "__main__":
     basedir = py.path.local(sys.argv[1], expanduser=True)
     serial = int(sys.argv[2])
     assert basedir.check(), basedir
     keyfs = KeyFS(basedir)
-    print (keyfs._fs.get_changelog_entry(serial))
+    pprint.pprint (keyfs._fs.get_changelog_entry(serial))
 
