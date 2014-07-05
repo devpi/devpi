2f342796cc07bc8690575501b2354fc8182d50d4
holger krekel <holger@merlinux.eu>
2014-07-05 21:15:27 +0200

    improve helper script for looking at changelog

--- server/extra/get_changelog.py
+++ server/extra/get_changelog.py
@@ -1,12 +1,24 @@
 import sys
 import py
 from devpi_server.keyfs import KeyFS
+import argparse
 import pprint
 
 if __name__ == "__main__":
-    basedir = py.path.local(sys.argv[1], expanduser=True)
-    serial = int(sys.argv[2])
-    assert basedir.check(), basedir
+    tw = py.io.TerminalWriter()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--serverdir",  type=str,
+            default="~/.devpi/server",
+            help="serverdir directory to look at")
+    parser.add_argument("changelog",  type=int, default=None,
+            help="changelog to look at", nargs="?")
+    args = parser.parse_args()
+    basedir = py.path.local(args.serverdir, expanduser=True)
+    assert basedir.exists()
     keyfs = KeyFS(basedir)
-    pprint.pprint (keyfs._fs.get_changes(serial))
+    tw.line("keyfs at %s" % basedir, bold=True)
+    if args.changelog is not None:
+        pprint.pprint(keyfs._fs.get_changes(args.changelog))
+    else:
+        tw.line("number of changelogs: %s" % keyfs.get_next_serial())
 
