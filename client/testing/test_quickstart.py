a3b9ac292982c0ee2239cbeafee05f06e66b0541
Florian Schulze <florian.schulze@gmx.net>
2014-07-07 14:10:31 +0200

    Use the custom X-Devpi-Auth header for authentication, instead of overwriting the Authentication header.

--- client/testing/test_quickstart.py
+++ client/testing/test_quickstart.py
@@ -1,9 +1,13 @@
 import py
+import pytest
 import subprocess
+
 
 def test_dryrun(cmd_devpi):
     cmd_devpi("quickstart", "--dry-run")
 
+
+@pytest.mark.skipif("config.option.fast")
 def test_functional(cmd_devpi, monkeypatch, tmpdir):
     monkeypatch.setenv("DEVPI_SERVERDIR", tmpdir.join("server"))
     monkeypatch.setenv("DEVPI_CLIENTDIR", tmpdir.join("client"))
