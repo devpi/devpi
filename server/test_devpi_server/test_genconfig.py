e0589fdb605b047c6d304da12edca5e9dd74ed80
holger krekel <holger@merlinux.eu>
2014-07-09 16:36:39 +0200

    fix according to @fschulze comments

--- server/test_devpi_server/test_genconfig.py
+++ server/test_devpi_server/test_genconfig.py
@@ -1,6 +1,12 @@
 import subprocess
 
-def test_gen_config_all():
+def test_gen_config_all(tmpdir):
+    tmpdir.chdir()
     proc = subprocess.Popen(["devpi-server", "--gen-config"])
     res = proc.wait()
     assert res == 0
+    b = tmpdir.join("gen-config")
+    assert b.join("supervisor-devpi.conf").check()
+    assert b.join("nginx-devpi.conf").check()
+    assert b.join("crontab").check()
+
