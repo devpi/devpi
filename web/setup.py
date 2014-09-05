f42d685c0ff66b364d2d41f368e9ce95c574a424
Florian Schulze <florian.schulze@gmx.net>
2014-09-05 13:55:02 +0200

    Require devpi-server 2.0.6 for the version info.

--- web/setup.py
+++ web/setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'Whoosh',
         'beautifulsoup4>=4.3.2',
-        'devpi-server>=2.0.0',
+        'devpi-server>=2.0.6',
         'docutils>=0.11',
         'pygments>=1.6',
         'pyramid',
