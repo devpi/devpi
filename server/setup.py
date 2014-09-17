03cb835d3872f12cfed03cccc1ad58d733f673a9
holger krekel <holger@merlinux.eu>
2014-09-17 14:14:34 +0200

    reshuffle changelog entries and add "upload history" changelog

--- server/setup.py
+++ server/setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
       keywords="pypi cache server wsgi",
       long_description=README,
       url="http://doc.devpi.net",
-      version='2.1.0.dev1',
+      version='2.1.0.dev2',
       maintainer="Holger Krekel",
       maintainer_email="holger@merlinux.eu",
       packages=find_packages(),
