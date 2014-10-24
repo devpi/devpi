f41d11aa9b8a0bf461869b8d31dc787dd4e80994
holger krekel <holger@merlinux.eu>
2014-10-24 17:24:33 +0200

    finalize 2.1.2

--- web/setup.py
+++ web/setup.py
@@ -5,7 +5,7 @@ setup(
     name="devpi-web",
     description="devpi-web: a web view for devpi-server",
     url="http://doc.devpi.net",
-    version='2.2.1.dev1',
+    version='2.2.1',
     maintainer="Holger Krekel, Florian Schulze",
     maintainer_email="holger@merlinux.eu",
     license="MIT",
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'Whoosh',
         'beautifulsoup4>=4.3.2',
-        'devpi-server>=2.1.2.dev2',
+        'devpi-server>=2.1.2',
         'devpi-common>=2.0.3',
         'docutils>=0.11',
         'pygments>=1.6',
