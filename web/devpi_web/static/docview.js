373c5e80c6e9dac9a099e4acf8c3499c8d3eda64
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-04-19 02:14:57 +0200

    js: updateIFrame() on window resize

--- web/devpi_web/static/docview.js
+++ web/devpi_web/static/docview.js
@@ -1,7 +1,14 @@
-function onIFrameLoad(iframe) {
+function updateIFrame(iframe) {
     // place iframe's parent div below navigation
-    var nav = $('div#navigation');
-    $(iframe).parent().css('top', nav.offset().top + nav.outerHeight());
+    var $nav = $('div#navigation');
+    $(iframe).parent().css('top', $nav.offset().top + $nav.outerHeight(true));
+}
+
+function onIFrameLoad(iframe) {
+    updateIFrame(iframe);
+    $(window).resize(function () {
+        updateIFrame(iframe);
+    });
     // copy title
     var title = $(iframe.contentWindow.document).find('title').text();
     if (title) {
