e02b2441d51f89ef8fbabd1695827b7b9f126f6a
Florian Schulze <florian.schulze@gmx.net>
2015-09-08 12:23:43 +0200

    Add changelog of last 5 releases to PyPI description.

--- common/setup.py
+++ common/setup.py
@@ -1,18 +1,30 @@
 #! /usr/bin/env python
 
 import os
+import re
 
 from setuptools import setup, find_packages
+
+
+def get_changelog():
+    text = open(os.path.join(here, 'CHANGELOG')).read()
+    header_matches = list(re.finditer('^-+$', text, re.MULTILINE))
+    # until fifth header
+    text = text[:header_matches[5].start()]
+    # all lines without fifth release number
+    lines = text.splitlines()[:-1]
+    return "Changelog\n=========\n\n" + "\n".join(lines)
+
 
 if __name__ == "__main__":
     here = os.path.abspath(".")
     README = open(os.path.join(here, 'README.rst')).read()
-    CHANGES = open(os.path.join(here, 'CHANGELOG')).read()
+    CHANGELOG = get_changelog()
 
     setup(
       name="devpi-common",
       description="utilities jointly used by devpi-server and devpi-client",
-      long_description=open("README.rst").read(),
+      long_description="\n\n".join([README, CHANGELOG]),
       version='2.0.6',
       packages=find_packages(),
       install_requires=["requests>=2.3.0", "py>=1.4.20"],
--- web/setup.py
+++ web/setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup
+import os
+import re
+
+
+def get_changelog():
+    here = os.path.abspath(".")
+    text = open(os.path.join(here, 'CHANGELOG')).read()
+    header_matches = list(re.finditer('^-+$', text, re.MULTILINE))
+    # until fifth header
+    text = text[:header_matches[5].start()]
+    # all lines without fifth release number
+    lines = text.splitlines()[:-1]
+    return "Changelog\n=========\n\n" + "\n".join(lines)
+
+
+README = open(os.path.abspath('README.rst')).read()
+CHANGELOG = get_changelog()
 
 
 setup(
     name="devpi-web",
     description="devpi-web: a web view for devpi-server",
+    long_description="\n\n".join([README, CHANGELOG]),
     url="http://doc.devpi.net",
     version='2.4.1.dev0',
     maintainer="Holger Krekel, Florian Schulze",
