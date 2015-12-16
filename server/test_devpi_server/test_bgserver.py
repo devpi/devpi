53f211d512e7593a995c4268bf1b79e291c57809
Tjerk Santegoeds <tsan@tdias.com>
2015-12-16 01:59:04 +0100

    Ignore http(s) proxy when starting the devpi server.

--- server/test_devpi_server/test_bgserver.py
+++ server/test_devpi_server/test_bgserver.py
@@ -1,8 +1,10 @@
 import sys
+import os
 import random
 import py
 import pytest
 from devpi_server.main import main
+from devpi_server.bgserver import no_proxy
 
 
 @pytest.mark.skipif("not config.option.slow")
@@ -20,3 +22,18 @@ def test_server_commands(tmpdir, monkeypatch):
             main(["devpi-server", "--start", portopt])
     finally:
         main(["devpi-server", "--stop"])
+
+
+def test_no_proxy():
+    original = {
+        "no_proxy": "no_proxy",
+        "NO_PROXY": "NO_PROXY",
+    }
+    os.environ.update(original)
+
+    with no_proxy("localhost:8080"):
+        assert os.environ["no_proxy"] == "localhost:8080"
+        assert "NO_PROXY" not in os.environ
+
+    for k, v in original.items():
+        assert os.environ[k] == v
