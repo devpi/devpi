f68dd9afa4accb26135a81cc6e10e3526991fe24
Florian Schulze <florian.schulze@gmx.net>
2016-05-13 17:14:16 +0200

    Prepare client 2.6.3.

--- client/setup.py
+++ client/setup.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     CHANGELOG = get_changelog()
 
     install_requires=["tox>=1.7.1",
-                      "devpi_common>2.0.2,<3.0",
+                      "devpi_common>2.0.2,<4.0",
                       "pkginfo>=1.2b1",
                       "check-manifest>=0.28",
                       "py>=1.4.31"]
@@ -57,7 +57,7 @@ if __name__ == "__main__":
       description="devpi upload/install/... workflow commands for Python "
                   "developers",
       long_description="\n\n".join([README, CHANGELOG]),
-      version='2.6.2',
+      version='2.6.3',
       packages=find_packages(),
       install_requires=install_requires,
       extras_require=extras_require,
