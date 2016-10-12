fb5578d9d4fb56b7523b003b8aa9d28454068cbd
Florian Schulze <florian.schulze@gmx.net>
2016-10-12 17:56:56 +0200

    Replace all occurrences of capturelog with catchlog.

--- server/test_devpi_server/conftest.py
+++ server/test_devpi_server/conftest.py
@@ -75,7 +75,7 @@ def Queue():
 
 @pytest.fixture()
 def caplog(caplog):
-    """ enrich the pytest-capturelog funcarg. """
+    """ enrich the pytest-catchlog funcarg. """
     def getrecords(msgrex=None, minlevel="DEBUG"):
         if msgrex is not None:
             msgrex = re.compile(msgrex)
