2ce79aa1828e9fe056d8a690bb42e9732d6da86a
Allan Feldman <afeldman@newrelic.com>
2017-03-22 13:06:27 -0700

    Addresses #270 by adding retry logic to replica connections.
    Under certain network conditions, it's possible for a connection from devpi to
    replicas (such as pypi) to be dropped.
    
    Before this commit, when this condition happens a FatalResponse exception is
    generated resulting in a "502 bad gateway" error being returned by devpi to the
    client. In many cases, this network error is transient and can be resolved by
    retrying the request to the replica.
    
    Currently, the requests library defaults to 0 retries for connection errors.
    
    This commit updates devpi-common and devpi-server to enable a retry limit other
    than 0. The retry limit is now specified in the configuration flag
    "--replica-max-retries" and defaults to 5 retries.

--- server/devpi_server/main.py
+++ server/devpi_server/main.py
@@ -262,14 +262,18 @@ class XOM:
             self.thread_pool.register(keyfs.notifier)
         return keyfs
 
-    def new_http_session(self, component_name):
-        session = new_requests_session(agent=(component_name, server_version))
+    def new_http_session(self, component_name, max_retries=None):
+        try:
+            session = new_requests_session(agent=(component_name, server_version), max_retries=max_retries)
+        except TypeError:
+            # Backward compatability with older devpi-common
+            session = new_requests_session(agent=(component_name, server_version))
         session.cert = self.config.args.replica_cert
         return session
 
     @cached_property
     def _httpsession(self):
-        return self.new_http_session("server")
+        return self.new_http_session("server", max_retries=self.config.args.replica_max_retries)
 
     def httpget(self, url, allow_redirects, timeout=30, extra_headers=None):
         if self.config.args.offline_mode:
