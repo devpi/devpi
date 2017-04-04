6feabd517fd9d212e93d2114af141a969bd6b879
Florian Schulze <florian.schulze@gmx.net>
2017-04-04 21:10:47 +0200

    Drop support for devpi-server < 4.0.0.
    Most things will still work with older versions, because the API didn't change yet.
    devpi-server < 4 is unlikely to be used in production anymore, because PyPI had too many changes.

--- client/setup.py
+++ client/setup.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
       description="devpi upload/install/... workflow commands for Python "
                   "developers",
       long_description="\n\n".join([README, CHANGELOG]),
-      version='2.8.0.dev0',
+      version='3.0.0.dev0',
       packages=find_packages(),
       install_requires=install_requires,
       extras_require=extras_require,
