b933c41d5d102b23c4c3219642729a664b4e5d53
Florian Schulze <florian.schulze@gmx.net>
2017-11-23 11:45:32 +0100

    Prepare releases.

--- client/setup.py
+++ client/setup.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
       description="devpi upload/install/... workflow commands for Python "
                   "developers",
       long_description="\n\n".join([README, CHANGELOG]),
-      version='3.1.0rc1',
+      version='3.1.0',
       packages=find_packages(),
       install_requires=install_requires,
       extras_require=extras_require,
