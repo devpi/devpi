bb33c8e978c2b5894779c988135935bd882ef6f1
Florian Schulze <mail@florian-schulze.net>
2017-12-11 07:59:27 +0100

    (fschulze, gotcha) Fix issue482: let external links in documentation open outside of iframe.

--- web/devpi_web/static/docview.js
+++ web/devpi_web/static/docview.js
@@ -1,22 +1,22 @@
-function onIFrameLoad(iframe) {
-    var $body = $('body'),
-        $header = $('.header'),
+function updateHeaderMargins($header, $body, $doc, $docHtml) {
+    // create enough space for letting devpi header overlap iframe
+    // without hiding iframe content
+    $docHtml.css('margin-top', $header.outerHeight(true));
+    // make scrollbar visible by adding a margin to the header
+    $header.css("margin-right", $body.width() - $doc.width());
+}
+
+
+function onIFrameLoad(event) {
+    var $body = event.data.$body,
+        $header = event.data.$header,
+        iframe = this,
         $doc = $(iframe.contentWindow.document),
         $docHtml = $doc.find('html');
 
-    function update() {
-        // create enough space for letting devpi header overlap iframe
-        // without hiding iframe content
-        $docHtml.css('margin-top', $header.outerHeight(true));
-        // make scrollbar visible by adding a margin to the header
-        $header.css("margin-right", $body.width() - $doc.width());
-    }
-    update();
-    $(window).resize(update);
-
     // make devpi header move away on iframe down-scrolling
     // and reappear on up-scrolling...
-    $doc.scroll(function () {
+    function scroll() {
         var headerTop = -$doc.scrollTop(),
             headerHeight = $header.outerHeight(true);
         // move header along with iframe scrolling...
@@ -25,7 +25,43 @@ function onIFrameLoad(iframe) {
             headerTop = -headerHeight;
         }
         $header.css('top', headerTop);
-        update();
+        updateHeaderMargins($header, $body, $doc, $docHtml)
+    }
+
+    // initialize header size and position on first load
+    scroll();
+
+    // update header position on scroll
+    $doc.scroll(scroll);
+
+    // fixup link target, so external links are opened outside the window
+    var base_url = $('iframe').data('base_url');
+    var $docBase = $doc.find('base');
+    if ($docBase.length == 0) {
+        var $docHead = $doc.find('head');
+        if ($docHead.length == 0) {
+            $docHead = $('<head></head>').prependTo($doc);
+        }
+        $docBase = $('<base>').prependTo($docHead);
+    }
+    // we add only a target attribute to the base tag, so by default links open
+    // outside the iframe
+    $docBase.attr('target', '_top');
+    // we use the click and submit events to catch stuff added via the dynamic
+    // Sphinx search
+    $docHtml.on('click', 'a[href]', function(e) {
+        var link = this;
+        if (link.href.indexOf(base_url) == 0) {
+            // let internal links still open inside the iframe
+            link.target = '_self';
+        }
+    });
+    $docHtml.on('submit', 'form', function(e) {
+        var form = this;
+        if (form.action.indexOf(base_url) == 0) {
+            // let internal forms still open inside the iframe
+            form.target = '_self';
+        }
     });
 
     // copy title from iframe's inner document
