9eb0bb265bd42ca78996acd297028570a32bff5f
Florian Schulze <mail@florian-schulze.net>
2018-02-02 15:20:39 +0100

    Prepare devpi-client 4.0.1 release.

--- client/setup.py
+++ client/setup.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
       description="devpi upload/install/... workflow commands for Python "
                   "developers",
       long_description="\n\n".join([README, CHANGELOG]),
-      version='4.0.0',
+      version='4.0.1',
       packages=find_packages(),
       install_requires=install_requires,
       extras_require=extras_require,
