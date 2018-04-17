60511838f0cd84aee80a3109cb939ce36096f495
Florian Schulze <mail@florian-schulze.net>
2018-04-17 08:19:37 +0200

    Fix Opensearch URL.

--- doc/conf.py
+++ doc/conf.py
@@ -163,7 +163,7 @@ html_show_sphinx = False
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-html_use_opensearch = 'http://devpi.net/'
+html_use_opensearch = 'https://doc.devpi.net'
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = None
