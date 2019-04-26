5b231a2bf640abe989c282b83aa21837f9e41297
Florian Schulze <mail@florian-schulze.net>
2019-04-26 11:15:40 +0200

    Fix for testing with older devpi-server.

--- client/testing/test_quickstart.py
+++ client/testing/test_quickstart.py
@@ -10,7 +10,13 @@ def test_dryrun(cmd_devpi):
 
 @pytest.mark.skipif("config.option.fast")
 def test_functional(cmd_devpi, monkeypatch, tmpdir):
-    monkeypatch.setenv("DEVPISERVER_SERVERDIR", tmpdir.join("server").strpath)
+    from devpi_server import __version__ as devpi_server_version
+    import pkg_resources
+    server_version = pkg_resources.parse_version(devpi_server_version)
+    if server_version >= pkg_resources.parse_version('4.7.0.dev'):
+        monkeypatch.setenv("DEVPISERVER_SERVERDIR", tmpdir.join("server").strpath)
+    else:
+        monkeypatch.setenv("DEVPI_SERVERDIR", tmpdir.join("server").strpath)
     monkeypatch.setenv("DEVPI_CLIENTDIR", tmpdir.join("client").strpath)
     cmd_devpi("quickstart")
     try:
