aa2558e435125e2fb6c8ae9cc7b7464d0fe7e34b
Florian Schulze <mail@florian-schulze.net>
2019-08-06 12:13:07 +0200

    Remove obsolete methods.

--- client/testing/test_pypirc.py
+++ client/testing/test_pypirc.py
@@ -1,6 +1,6 @@
-
 from devpi import pypirc
 from textwrap import dedent
+
 
 def test_pypirc(tmpdir):
     p = tmpdir.join("pypirc")
@@ -20,19 +20,13 @@ def test_pypirc(tmpdir):
         password: test2
     """))
     rc = pypirc.Auth(p)
-    assert rc.validate_user("http://pypi.testrun.org/", "test", "test")
-    assert not rc.validate_user("http://pypi.testrun.org/", "test", "test2")
-    assert rc.validate_user("http://localhost:3141/", "test2", "test2")
-    assert not rc.validate_user("http://localhost:3141/", "test", "test")
-
-    userpass = rc.get_userpass("http://pypi.testrun.org/")
-    assert userpass == ("test", "test")
-    userpass = rc.get_userpass("http://localhost:3141/")
-    assert userpass == ("test2", "test2")
-    userpass = rc.get_userpass("http://qwleklocalhost:3141/")
-    assert userpass is None
 
     url, (user, p) = rc.get_url_auth("local")
     assert url == "http://pypi.testrun.org/"
     assert user == "test"
     assert p == "test"
+
+    url, (user, p) = rc.get_url_auth("testindex")
+    assert url == "http://localhost:3141/"
+    assert user == "test2"
+    assert p == "test2"
