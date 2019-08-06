9395a57e84bce698dc5a2fdf154284591dc6ce76
Graham Williamson <graham@williamsonsinc.id.au>
2019-08-06 10:54:02 +1000

    Adds missing support python version

--- postgresql/setup.py
+++ postgresql/setup.py
@@ -43,7 +43,7 @@ setup(
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"] + [
             "Programming Language :: Python :: %s" % x
-            for x in "3.4 3.5 3.6 3.7".split()],
+            for x in "2.7 3.4 3.5 3.6 3.7".split()],
     entry_points={
         'devpi_server': [
             "devpi-postgresql = devpi_postgresql.main"],
