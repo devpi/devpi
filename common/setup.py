b1a8098730e8b5795ee2fbdad9808a5f52a2709a
Manish Kumar <manish.kumar@onelan.com>
2019-09-07 15:12:44 +0530

    Add python 3.6 version to the classifiers

--- common/setup.py
+++ common/setup.py
@@ -45,6 +45,7 @@ if __name__ == "__main__":
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         ],
       )
