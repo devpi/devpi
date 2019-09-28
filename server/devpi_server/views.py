934016ff174227d92cf9aaaf7d3ce962079b4dd2
Florian Schulze <mail@florian-schulze.net>
2019-09-28 10:43:54 +0200

    Fix display of master uuid in +status.

--- server/devpi_server/views.py
+++ server/devpi_server/views.py
@@ -296,7 +296,7 @@ class StatusView:
             from .replica import ReplicationErrors
             status["role"] = "REPLICA"
             status["master-url"] = config.master_url.url
-            status["master-uuid"] = config.nodeinfo.get("master-uuid")
+            status["master-uuid"] = config.get_master_uuid()
             status["master-serial"] = self.xom.replica_thread.get_master_serial()
             status["master-serial-timestamp"] = self.xom.replica_thread.get_master_serial_timestamp()
             status["replica-started-at"] = self.xom.replica_thread.started_at
