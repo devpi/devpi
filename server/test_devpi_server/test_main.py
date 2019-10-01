84169fd534650f981f1395a181ea0ffd20067fdf
Florian Schulze <mail@florian-schulze.net>
2019-10-01 14:33:14 +0200

    Make test independent from number of middlewares.

--- server/test_devpi_server/test_main.py
+++ server/test_devpi_server/test_main.py
@@ -190,8 +190,10 @@ def test_main_starts_server_if_run_commands_returns_none(tmpdir, use_option):
 
 def test_version_info(xom):
     app = xom.create_app()
+    while not hasattr(app, 'registry'):
+        app = app.app
     counts = {}
-    for name, version in app.app.registry['devpi_version_info']:
+    for name, version in app.registry['devpi_version_info']:
         counts[name] = counts.get(name, 0) + 1
     assert counts['devpi-server'] == 1
 
