18636ae4844e6210f9b0097e98238f1266a5f08d
Florian Schulze <mail@florian-schulze.net>
2020-11-02 10:21:02 +0100

    Add tests for current index list behaviour.

--- client/testing/test_index.py
+++ client/testing/test_index.py
@@ -1,4 +1,4 @@
-from devpi.index import index_show
+from devpi.index import index_show, main
 import pytest
 
 
@@ -6,3 +6,62 @@ def test_index_show_empty(loghub):
     with pytest.raises(SystemExit):
         index_show(loghub, None)
     loghub._getmatcher().fnmatch_lines("*no index specified*")
+
+
+def test_index_list_without_login(loghub):
+    loghub.args.indexname = None
+    loghub.args.keyvalues = []
+    loghub.args.list = True
+    with pytest.raises(SystemExit):
+        main(loghub, loghub.args)
+    loghub._getmatcher().fnmatch_lines("*you need to be logged in*")
+
+
+def test_index_list_with_login_and_no_username(loghub, mock_http_api):
+    loghub.current.reconfigure(dict(
+        simpleindex="http://devpi/index",
+        index="http://devpi/root/dev/",
+        login="http://devpi/+login"))
+    loghub.current.set_auth("hello", "pass1")
+    loghub.args.indexname = None
+    loghub.args.keyvalues = []
+    loghub.args.list = True
+    mock_http_api.set("http://devpi/hello", 200, result={
+        "username": "hello",
+        "indexes": {"foo": None}})
+    main(loghub, loghub.args)
+    assert list(filter(None, loghub._getmatcher().lines)) == ["hello/foo"]
+
+
+def test_index_list_with_login_and_username(loghub, mock_http_api):
+    loghub.current.reconfigure(dict(
+        simpleindex="http://devpi/index",
+        index="http://devpi/root/dev/",
+        login="http://devpi/+login"))
+    loghub.current.set_auth("hello", "pass1")
+    loghub.args.indexname = "root"
+    loghub.args.keyvalues = []
+    loghub.args.list = True
+    # the username is currently ignored
+    mock_http_api.set("http://devpi/hello", 200, result={
+        "username": "hello",
+        "indexes": {"foo": None}})
+    main(loghub, loghub.args)
+    assert list(filter(None, loghub._getmatcher().lines)) == ["hello/foo"]
+
+
+def test_index_list_with_login_and_indexname(loghub, mock_http_api):
+    loghub.current.reconfigure(dict(
+        simpleindex="http://devpi/index",
+        index="http://devpi/root/dev/",
+        login="http://devpi/+login"))
+    loghub.current.set_auth("hello", "pass1")
+    loghub.args.indexname = "root/dev"
+    loghub.args.keyvalues = []
+    loghub.args.list = True
+    # the indexname is currently ignored
+    mock_http_api.set("http://devpi/hello", 200, result={
+        "username": "hello",
+        "indexes": {"foo": None}})
+    main(loghub, loghub.args)
+    assert list(filter(None, loghub._getmatcher().lines)) == ["hello/foo"]
