82275d6fd2a8ef1b592737b0296fa648ec3eef38
Florian Schulze <mail@florian-schulze.net>
2020-12-13 09:50:59 +0100

    Run uninstall and install commands in one step each.

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -exu
 
 if test "$VIRTUAL_ENV" == ""; then
     echo No active virtualenv detected
     exit 1
 fi
 
-for i in common client server web ; do
-    pip uninstall -y devpi-$i
-done
-for i in web server client common ; do
-    (cd $i ; pip install -U -e .)
-done
-
-# install some deps for testing
-pip install -U --upgrade-strategy eager PdbEditorSupport Sphinx pytest mock webtest pytest-cov pytest-flake8 pytest-pdb pytest-timeout beautifulsoup4 supervisor tox towncrier wheel certauth
+pip uninstall -y devpi-{common,client,server,web}
+pip install -U --upgrade-strategy eager -e common -e client -e server -e web PdbEditorSupport Sphinx pytest mock webtest pytest-cov pytest-flake8 pytest-pdb pytest-timeout beautifulsoup4 supervisor tox towncrier wheel certauth
