52431f089ea4c0fc1cd012302efe964078c5e1a8
Florian Schulze <mail@florian-schulze.net>
2021-01-12 11:22:37 +0100

    Pin tox to <4dev.
    The next major version of devpi-client will use tox as a binary to support newer versions.

--- client/setup.py
+++ client/setup.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     README = io.open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
     CHANGELOG = get_changelog()
 
-    install_requires=["tox>=3.1.0",
+    install_requires=["tox>=3.1.0,<4dev",
                       "devpi_common<4,>=3.6.0",
                       "pkginfo>=1.4.2",
                       "check-manifest>=0.28",
