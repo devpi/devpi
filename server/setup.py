e946d85f897509d73153cd69429aa7861a715626
Florian Schulze <mail@florian-schulze.net>
2021-03-01 14:01:43 +0100

    Fix version pin.

--- server/setup.py
+++ server/setup.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                         "devpi_common<4,>=3.3.0",
                         "itsdangerous>=0.24",
                         "execnet>=1.2",
-                        "pyramid>=1.8<2",
+                        "pyramid>=1.8,<2",
                         "waitress>=1.0.1",
                         "repoze.lru>=0.6",
                         "passlib[argon2]",
