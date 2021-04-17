99608eebf58a268b72e14cb2484350132296b766
Florian Schulze <mail@florian-schulze.net>
2021-04-17 15:51:35 +0200

    Bump.

--- server/setup.py
+++ server/setup.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         'Documentation': 'https://doc.devpi.net',
         'Source Code': 'https://github.com/devpi/devpi'
       },
-      version='6.0.0.dev27',
+      version='6.0.0rc1',
       maintainer="Holger Krekel, Florian Schulze",
       maintainer_email="holger@merlinux.eu",
       packages=[
