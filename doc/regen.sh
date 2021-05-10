81dbd7522b921fa8e9398d2a7e8c467451429a4d
Florian Schulze <mail@florian-schulze.net>
2021-05-10 16:40:34 +0200

    Fix generation of documentation.

--- doc/regen.sh
+++ doc/regen.sh
@@ -1,24 +1,33 @@
 #/bin/bash 
+set -e -x -o nounset
 
 export VENV_DIR=/tmp/docenv
 
-alias devpi-server=`which devpi-server`
-alias devpi=`which devpi`
-
 [ -z "$PIP_INDEX_URL" ] && exit "need to set PIP_INDEX_URL to an index that contains the latest devpi packages because especially quickstart-server doc regeneration depends on it"
 
-rm -rf ${VENV_DIR} TARGETDIR docenv v1 ${TMPDIR}/devpi*
-virtualenv -q ${VENV_DIR}
-. ${VENV_DIR}/bin/activate
-echo $PATH
+echo $(pwd)
+echo $TMPDIR
 
-pip install -q py
+rm -rf ${VENV_DIR} ${TMPDIR}/devpi*
+virtualenv -p python3 -q ${VENV_DIR}
+. ${VENV_DIR}/bin/activate
+echo $VIRTUAL_ENV
+echo $PATH
+export VIRTUAL_ENV
+export PATH
+
+echo `which pip`
+
+pip install -q -U py pip
 echo devpi-server: `which devpi-server` $*
 
 regendoc \
     --verbose \
     --normalize "/[ \t]+\n/\n/" \
+    --normalize "@generated uuid: [0-9a-f]+@generated uuid: 446e22e0db5e41a5989fd671e98ec30b@" \
     --normalize "@\\\$PYTHON_PREFIX@/home/devpi/devpi@" \
     --normalize "@\\\$REGENDOC_TMPDIR@/home/devpi/devpi/doc@" \
-    --normalize "@\\\/private/home@/home@" \
+    --normalize "@$TMPDIR@/tmp/@" \
+    --normalize "@/private/home@/home@" \
+    --normalize "@/private/tmp/docenv@/tmp/docenv@" \
     $*
