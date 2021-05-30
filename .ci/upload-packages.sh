f98a626a39420abd66c813d39ee9ef429147669c
Florian Schulze <mail@florian-schulze.net>
2021-05-30 12:26:38 +0200

    Switch to GitHub Actions.

---
+++ .ci/upload-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -xe -o nounset
+yes yes | devpi index --delete "${DEVPI_INDEX}" || true
+devpi index -c "${DEVPI_INDEX}" bases=root/pypi
+devpi use "${DEVPI_INDEX}"
+devpi push --index root/pypi devpi-server==4.0.0 "${DEVPI_INDEX}"
+devpi push --index root/pypi devpi-server==5.2.0 "${DEVPI_INDEX}"
+pushd common
+yes | towncrier
+devpi upload
+popd
+pushd server
+yes | towncrier
+devpi upload
+popd
+pushd web
+yes | towncrier
+devpi upload
+popd
+pushd client
+yes | towncrier
+devpi upload
+popd
+pushd postgresql
+yes | towncrier
+devpi upload
+popd
