599b0d5a4082b14ea9751954a455fd8b68b76a3f
Florian Schulze <mail@florian-schulze.net>
2021-06-23 16:18:12 +0200

    Prepare devpi-server 6.0.1 release.

--- server/setup.py
+++ server/setup.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         'Documentation': 'https://doc.devpi.net',
         'Source Code': 'https://github.com/devpi/devpi'
       },
-      version='6.0.0',
+      version='6.0.1',
       maintainer="Holger Krekel, Florian Schulze",
       maintainer_email="holger@merlinux.eu",
       packages=[
