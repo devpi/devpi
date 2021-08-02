185072fcc6f927232cd4bc12b55b9bdbf4b72968
Florian Schulze <mail@florian-schulze.net>
2021-08-02 18:10:39 +0200

    Bump upper pin for pluggy version.
    There are no deprecation warnings with the current development version and tests pass.

--- server/setup.py
+++ server/setup.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                         "waitress>=1.0.1",
                         "repoze.lru>=0.6",
                         "passlib[argon2]",
-                        "pluggy>=0.6.0,<1.0",
+                        "pluggy>=0.6.0,<2.0",
                         'ruamel.yaml',
                         "strictyaml",
                         ]
