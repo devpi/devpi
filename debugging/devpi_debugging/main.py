3ba7289ff8315e882cea0d45548681b123024402
Florian Schulze <mail@florian-schulze.net>
2021-08-10 12:34:52 +0200

    Add poking thread to debugging for identifying long running requests.

--- debugging/devpi_debugging/main.py
+++ debugging/devpi_debugging/main.py
@@ -1,7 +1,21 @@
+from devpi_server.log import threadlog
+from devpi_server.log import thread_push_log
 from pluggy import HookimplMarker
+from pyramid.events import NewRequest
+from pyramid.events import subscriber
+import os
+import sys
+import threading
+import time
 
 
 hookimpl = HookimplMarker("devpiserver")
+
+
+@subscriber(NewRequest)
+def mysubscriber(event):
+    event.request._devpi_debugging_start_time = time.time()
+    event.request._devpi_debugging_next_time_delta = 1
 
 
 @hookimpl
@@ -10,6 +24,17 @@ def devpiserver_add_parser_options(parser):
     debugging.addoption(
         "--debug-keyfs", action="store_true",
         help="enable the +keyfs views")
+    debugging.addoption(
+        "--debug-poke", action="store_true",
+        help="enable poking thread which will periodically log a stack trace "
+             "of long running requests and other non sleeping threads")
+
+
+@hookimpl
+def devpiserver_cmdline_run(xom):
+    if xom.config.args.debug_poke:
+        xom.thread_pool.register(PokingThread(xom))
+    return None
 
 
 @hookimpl
@@ -25,3 +50,122 @@ def includeme(config):
         "keyfs_changelog",
         "/+keyfs/{serial}")
     config.scan()
+
+
+def iter_frame_stack_info(frame):
+    while frame.f_back:
+        co = frame.f_code
+        name = co.co_name
+        (path, filename) = os.path.split(co.co_filename)
+        (path, basename) = os.path.split(path)
+        yield (frame, co, name, path, basename, filename)
+        frame = frame.f_back
+
+
+def iter_current_threads(skip_thread_ids):
+    frames = sys._current_frames()
+    for thread_id, frame in frames.items():
+        for f, co, name, path, basename, filename in iter_frame_stack_info(frame):
+            if thread_id in skip_thread_ids:
+                yield (thread_id, frame, "skipped")
+                break
+            if name == "wait" and filename == "threading.py" and basename.startswith("python"):
+                continue
+            elif name == "handler_thread" and filename == "task.py" and basename == "waitress":
+                yield (thread_id, frame, "known_waiting")
+            elif name == "select" and filename == "selectors.py" and basename.startswith("python"):
+                continue
+            elif name == "db_read_last_changelog_serial" and basename == "devpi_server":
+                continue
+            elif name == "sleep" and filename == "mythread.py" and basename == "devpi_server":
+                yield (thread_id, frame, "known_waiting")
+            elif name == "wait_tx_serial" and filename == "keyfs.py" and basename == "devpi_server":
+                yield (thread_id, frame, "known_waiting")
+            elif name == "_run_once" and filename == "base_events.py" and basename == "asyncio":
+                yield (thread_id, frame, "known_waiting")
+            elif name == "_worker" and filename == "thread.py" and basename == "futures":
+                yield (thread_id, frame, "known_waiting")
+            elif name == "poll" and filename == "wasyncore.py" and basename == "waitress":
+                yield (thread_id, frame, "known_waiting")
+            elif name == "wait_tx_serial" and filename == "keyfs.py" and basename == "devpi_server":
+                yield (thread_id, frame, "known_waiting")
+            else:
+                yield (thread_id, frame, "unknown")
+            break
+
+
+class PokingThread:
+    def __init__(self, xom):
+        self.xom = xom
+        self.thread_ids = {threading.get_ident()}
+        self.thread_names = {}
+
+    def tick(self):
+        for thread_id, frame, status in iter_current_threads(self.thread_ids):
+            if status in {"known_waiting", "skipped"}:
+                continue
+            skipped = False
+            request = None
+            stack = []
+            for f, co, name, path, basename, filename in iter_frame_stack_info(frame):
+                if name == "queue_projects" and filename == "whoosh_index.py" and basename == "devpi_web":
+                    # found the indexer queuing thread
+                    self.thread_ids.add(thread_id)
+                    skipped = True
+                    break
+                if name == "process_next" and filename == "whoosh_index.py" and basename == "devpi_web":
+                    # found the indexer thread
+                    self.thread_ids.add(thread_id)
+                    skipped = True
+                    break
+                if name == "process_next" and filename == "replica.py" and basename == "devpi_server":
+                    # found a file replication thread
+                    self.thread_ids.add(thread_id)
+                    skipped = True
+                    break
+                if name == "thread_run":
+                    if thread_id not in self.thread_names:
+                        thread_self = f.f_locals.get('self')
+                        if thread_self is None:
+                            self.thread_names[thread_id] = None
+                        else:
+                            self.thread_names[thread_id] = f"{thread_self.__class__.__module__}.{thread_self.__class__.__name__}"
+                    if filename == "replica.py" and basename == "devpi_server":
+                        if f.f_locals['self'].__class__.__name__ in {'ReplicaThread', 'InitialQueueThread'}:
+                            # found one of the replica threads
+                            self.thread_ids.add(thread_id)
+                            skipped = True
+                    # we don't need to go further up the stack
+                    break
+                if name == "invoke_request" and filename == "router.py" and basename == "pyramid":
+                    request = f.f_locals.get('request')
+                    # we got the request, so we don't need to go further up the stack
+                    break
+                stack.append(f"    {co.co_filename}:{f.f_lineno} {co.co_name}")
+            if skipped:
+                continue
+            stack = '\n'.join(reversed(stack))
+            if request is not None:
+                delta = time.time() - request._devpi_debugging_start_time
+                if delta < request._devpi_debugging_next_time_delta:
+                    continue
+                next_delta = request._devpi_debugging_next_time_delta * 1.25
+                request._devpi_debugging_next_time_delta += next_delta
+                stack = stack + f"\n  Next poke in {next_delta} for {request.method} {request.url} from {request.client_addr} ({request.user_agent})"
+            thread_name = self.thread_names.get(thread_id)
+            if thread_name is None:
+                thread_name = f"{thread_id}"
+            else:
+                thread_name = f"{thread_name} ({thread_id})"
+            threadlog.info(f"Thread {thread_name} is in:\n{stack}")
+
+    def thread_run(self):
+        self.thread_ids.add(threading.get_ident())
+        thread_push_log("[POKE]")
+        threadlog.info("Starting poking thread")
+        while 1:
+            self.thread.sleep(1)
+            try:
+                self.tick()
+            except Exception:
+                threadlog.exception("Error in poking thread:")
