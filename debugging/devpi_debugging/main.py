a6acfaf232b209db45c777ad6d542d55f79b04ac
Florian Schulze <mail@florian-schulze.net>
2021-08-10 13:08:13 +0200

    Add debugging USR1 signal handler to print current stack traces of all threads.

--- debugging/devpi_debugging/main.py
+++ debugging/devpi_debugging/main.py
@@ -1,9 +1,11 @@
 from devpi_server.log import threadlog
 from devpi_server.log import thread_push_log
+from devpi_server.main import fatal
 from pluggy import HookimplMarker
 from pyramid.events import NewRequest
 from pyramid.events import subscriber
 import os
+import signal
 import sys
 import threading
 import time
@@ -28,12 +30,22 @@ def devpiserver_add_parser_options(parser):
         "--debug-poke", action="store_true",
         help="enable poking thread which will periodically log a stack trace "
              "of long running requests and other non sleeping threads")
+    debugging.addoption(
+        "--debug-signal", action="store_true",
+        help="enable USR1 signal handler to log the current stack trace "
+             "of each thread")
 
 
 @hookimpl
 def devpiserver_cmdline_run(xom):
     if xom.config.args.debug_poke:
         xom.thread_pool.register(PokingThread(xom))
+    if xom.config.args.debug_signal:
+        current_handler = signal.getsignal(signal.SIGUSR1)
+        if current_handler == signal.SIG_DFL:
+            signal.signal(signal.SIGUSR1, show_stacks)
+        else:
+            fatal(f"Couldn't install USR1 signal handler, because another one is already active: {current_handler}")
     return None
 
 
@@ -67,31 +79,60 @@ def iter_current_threads(skip_thread_ids):
     for thread_id, frame in frames.items():
         for f, co, name, path, basename, filename in iter_frame_stack_info(frame):
             if thread_id in skip_thread_ids:
-                yield (thread_id, frame, "skipped")
+                yield (thread_id, frame, "skipped", None)
                 break
             if name == "wait" and filename == "threading.py" and basename.startswith("python"):
                 continue
             elif name == "handler_thread" and filename == "task.py" and basename == "waitress":
-                yield (thread_id, frame, "known_waiting")
+                yield (thread_id, frame, "known_waiting", "waitress handler")
             elif name == "select" and filename == "selectors.py" and basename.startswith("python"):
                 continue
             elif name == "db_read_last_changelog_serial" and basename == "devpi_server":
                 continue
             elif name == "sleep" and filename == "mythread.py" and basename == "devpi_server":
-                yield (thread_id, frame, "known_waiting")
+                yield (thread_id, frame, "known_waiting", "mythread sleeping")
             elif name == "wait_tx_serial" and filename == "keyfs.py" and basename == "devpi_server":
-                yield (thread_id, frame, "known_waiting")
+                yield (thread_id, frame, "known_waiting", "keyfs wait_tx_serial")
             elif name == "_run_once" and filename == "base_events.py" and basename == "asyncio":
-                yield (thread_id, frame, "known_waiting")
+                yield (thread_id, frame, "known_waiting", "asyncio _run_once")
             elif name == "_worker" and filename == "thread.py" and basename == "futures":
-                yield (thread_id, frame, "known_waiting")
+                yield (thread_id, frame, "known_waiting", "futures _worker")
             elif name == "poll" and filename == "wasyncore.py" and basename == "waitress":
-                yield (thread_id, frame, "known_waiting")
-            elif name == "wait_tx_serial" and filename == "keyfs.py" and basename == "devpi_server":
-                yield (thread_id, frame, "known_waiting")
+                yield (thread_id, frame, "known_waiting", "waitress poll")
             else:
-                yield (thread_id, frame, "unknown")
+                yield (thread_id, frame, "unknown", "unknown")
             break
+
+
+def show_stacks(signal, stack):
+    output = ["=" * 80]
+    for thread_id, frame, status, status_src in iter_current_threads({threading.get_ident()}):
+        if status == "skipped":
+            continue
+        request = None
+        stack = []
+        thread_name = f"{thread_id}"
+        for f, co, name, path, basename, filename in iter_frame_stack_info(frame):
+            if name == "thread_run":
+                thread_self = f.f_locals.get('self')
+                if thread_self is not None:
+                    thread_name = f"{thread_self.__class__.__module__}.{thread_self.__class__.__name__} ({thread_name})"
+                # we don't need to go further up the stack
+                break
+            if name == "invoke_request" and filename == "router.py" and basename == "pyramid":
+                request = f.f_locals.get('request')
+                # we got the request, so we don't need to go further up the stack
+                break
+            stack.append(f"    {co.co_filename}:{f.f_lineno} {co.co_name}")
+        if status == "known_waiting":
+            output.append(f"Thread {thread_name} is waiting ({status_src})")
+        else:
+            stack = '\n'.join(reversed(stack))
+            if request is not None:
+                delta = time.time() - request._devpi_debugging_start_time
+                stack = stack + f"\n  Request {request.method} {request.url} from {request.client_addr} ({request.user_agent}) running for {delta}"
+            output.append(f"Thread {thread_name} is in:\n{stack}")
+    print(*output, sep='\n', file=sys.stderr, flush=True)
 
 
 class PokingThread:
@@ -101,7 +142,7 @@ class PokingThread:
         self.thread_names = {}
 
     def tick(self):
-        for thread_id, frame, status in iter_current_threads(self.thread_ids):
+        for thread_id, frame, status, status_src in iter_current_threads(self.thread_ids):
             if status in {"known_waiting", "skipped"}:
                 continue
             skipped = False
