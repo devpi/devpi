715aada786c3067a3480677f3ea163ce7df1ca2d
Florian Schulze <mail@florian-schulze.net>
2021-12-04 12:46:37 +0100

    Fix #864: enable settings from ``~/.netrc`` and ``HTTP_PROXY``/``HTTPS_PROXY`` env vars for aiohttp.

--- server/setup.py
+++ server/setup.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         'Documentation': 'https://doc.devpi.net',
         'Source Code': 'https://github.com/devpi/devpi'
       },
-      version='6.3.0',
+      version='6.3.1',
       maintainer="Holger Krekel, Florian Schulze",
       maintainer_email="holger@merlinux.eu",
       packages=[
