fb613cd44fd868ed248c0569cc6289eb4dbc0959
Florian Schulze <mail@florian-schulze.net>
2022-03-16 11:38:11 +0100

    Use os.replace instead of os.rename for atomic renames with overwrite.

--- server/devpi_server/config.py
+++ server/devpi_server/config.py
@@ -14,6 +14,7 @@ from devpi_common.types import cached_property
 from distutils.util import strtobool
 from functools import partial
 from .log import threadlog
+from . import fileutil
 from . import hookspecs
 import json
 import devpi_server
@@ -678,11 +679,7 @@ class Config(object):
         prefix = "-" + self.path_nodeinfo.basename
         with NamedTemporaryFile(prefix=prefix, delete=False, dir=nodeinfo_dir.strpath) as f:
             f.write(json.dumps(self.nodeinfo, indent=2).encode('utf-8'))
-        try:
-            os.remove(self.path_nodeinfo.strpath)
-        except FileNotFoundError:
-            pass
-        os.rename(f.name, self.path_nodeinfo.strpath)
+        fileutil.rename(f.name, self.path_nodeinfo.strpath)
         threadlog.info("wrote nodeinfo to: %s", self.path_nodeinfo)
 
     @property
