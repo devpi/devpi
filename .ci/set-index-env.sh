16aabb7b0cffb801430e131ee08ef1fd70432afc
Florian Schulze <mail@florian-schulze.net>
2022-07-19 16:18:57 +0200

    Use DEVPI_INDEXNAME instead of DEVPI_INDEX.

--- .ci/set-index-env.sh
+++ .ci/set-index-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe -o nounset
 export GITHUB_BRANCH_NAME="${GITHUB_REF##*/}"
-export DEVPI_INDEX="devpi-github/${GITHUB_REPOSITORY/\//-}-${GITHUB_BRANCH_NAME/\//-}"
-export PIP_INDEX_URL="https://m.devpi.net/${DEVPI_INDEX}/+simple/"
+export DEVPI_INDEXNAME="devpi-github/${GITHUB_REPOSITORY/\//-}-${GITHUB_BRANCH_NAME/\//-}"
+export PIP_INDEX_URL="https://m.devpi.net/${DEVPI_INDEXNAME}/+simple/"
--- .ci/upload-packages.sh
+++ .ci/upload-packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe -o nounset
-yes yes | devpi index --delete "${DEVPI_INDEX}" || true
-devpi index -c "${DEVPI_INDEX}" bases=root/pypi
-devpi use "${DEVPI_INDEX}"
-devpi push --index root/pypi devpi-common==3.6.0 "${DEVPI_INDEX}"
-devpi push --index root/pypi devpi-server==5.2.0 "${DEVPI_INDEX}"
-devpi push --index root/pypi devpi-server==6.2.0 "${DEVPI_INDEX}"
+yes yes | devpi index --delete "${DEVPI_INDEXNAME}" || true
+devpi index -c "${DEVPI_INDEXNAME}" bases=root/pypi
+devpi use "${DEVPI_INDEXNAME}"
+devpi push --index root/pypi devpi-common==3.6.0 "${DEVPI_INDEXNAME}"
+devpi push --index root/pypi devpi-server==5.2.0 "${DEVPI_INDEXNAME}"
+devpi push --index root/pypi devpi-server==6.2.0 "${DEVPI_INDEXNAME}"
 pushd common
 yes | towncrier
 devpi upload
