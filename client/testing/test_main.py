d4518eee2947e76849091027c9f02ac0d2d10866
Florian Schulze <mail@florian-schulze.net>
2022-07-21 14:06:00 +0200

    Print found devpi-client plugins with --version.

--- client/testing/test_main.py
+++ client/testing/test_main.py
@@ -86,9 +86,11 @@ def test_pkgresources_version_matches_init():
     assert pkg_resources.get_distribution("devpi_client").version == ver
 
 
-def test_version(loghub):
+def test_version(loghub, monkeypatch):
+    from devpi.main import get_pluginmanager
     from devpi.main import print_version
     loghub.debug = lambda self, *msg: None
+    loghub.pm = get_pluginmanager(load_entry_points=False)
     print_version(loghub)
     lines = list(filter(None, loghub._getmatcher().lines))
     assert len(lines) == 1
@@ -103,6 +105,7 @@ def test_version_server(loghub, url_of_liveserver):
     print_version(loghub)
     lines = list(filter(None, loghub._getmatcher().lines))
     assert len(lines) > 2
+    loghub._getmatcher().fnmatch_lines("current devpi server:*")
     names = [x.strip().split()[0] for x in lines]
     assert 'devpi-client' in names
     assert 'devpi-server' in names
