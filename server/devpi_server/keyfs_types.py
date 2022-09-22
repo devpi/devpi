5328d179ef05a5f75b1e1662e96dfcbbaef66684
Florian Schulze <mail@florian-schulze.net>
2022-09-22 19:13:42 +0200

    Move RelpathInfo, PTypedKey and TypedKey to separate keyfs_types module.

---
+++ server/devpi_server/keyfs_types.py
@@ -0,0 +1,102 @@
+import attr
+import contextlib
+import py
+import re
+
+
+@attr.s(slots=True)
+class RelpathInfo(object):
+    relpath = attr.ib(type=str)
+    keyname = attr.ib(type=str)
+    serial = attr.ib(type=int)
+    back_serial = attr.ib(type=int)
+    value = attr.ib()
+
+
+class PTypedKey:
+    __slots__ = ('keyfs', 'name', 'pattern', 'rex_reverse', 'type')
+    rex_braces = re.compile(r'\{(.+?)\}')
+
+    def __init__(self, keyfs, key, type, name):
+        self.keyfs = keyfs
+        self.pattern = py.builtin._totext(key)
+        self.type = type
+        self.name = name
+
+        def repl(match):
+            name = match.group(1)
+            return r'(?P<%s>[^\/]+)' % name
+        rex_pattern = self.pattern.replace("+", r"\+")
+        rex_pattern = self.rex_braces.sub(repl, rex_pattern)
+        self.rex_reverse = re.compile("^" + rex_pattern + "$")
+
+    def __call__(self, **kw):
+        for val in kw.values():
+            if "/" in val:
+                raise ValueError(val)
+        relpath = self.pattern.format(**kw)
+        return TypedKey(self.keyfs, relpath, self.type, self.name,
+                        params=kw)
+
+    def extract_params(self, relpath):
+        m = self.rex_reverse.match(relpath)
+        return m.groupdict() if m is not None else {}
+
+    def on_key_change(self, callback):
+        self.keyfs.notifier.on_key_change(self.name, callback)
+
+    def __repr__(self):
+        return f"<PTypedKey {self.pattern!r} type {self.type.__name__!r}>"
+
+
+class TypedKey:
+    __slots__ = ('keyfs', 'name', 'params', 'relpath', 'type')
+
+    def __init__(self, keyfs, relpath, type, name, params=None):
+        self.keyfs = keyfs
+        self.relpath = relpath
+        self.type = type
+        self.name = name
+        self.params = params or {}
+
+    def __hash__(self):
+        return hash(self.relpath)
+
+    def __eq__(self, other):
+        return self.relpath == other.relpath
+
+    def __repr__(self):
+        return f"<TypedKey {self.name} {self.type.__name__} {self.relpath}>"
+
+    def get(self, readonly=True):
+        return self.keyfs.tx.get(self, readonly=readonly)
+
+    @property
+    def last_serial(self):
+        try:
+            return self.keyfs.tx.last_serial(self)
+        except KeyError:
+            return None
+
+    def is_dirty(self):
+        return self.keyfs.tx.is_dirty(self)
+
+    @contextlib.contextmanager
+    def update(self):
+        val = self.keyfs.tx.get(self, readonly=False)
+        yield val
+        # no exception, so we can set and thus mark dirty the object
+        self.set(val)
+
+    def set(self, val):
+        if not isinstance(val, self.type):
+            raise TypeError(
+                "%r requires value of type %r, got %r" % (
+                    self.relpath, self.type.__name__, type(val).__name__))
+        self.keyfs.tx.set(self, val)
+
+    def exists(self):
+        return self.keyfs.tx.exists(self)
+
+    def delete(self):
+        return self.keyfs.tx.delete(self)
