0661c41d6f8e90d22a5068b74639d03c0875255b
Florian Schulze <mail@florian-schulze.net>
2022-12-05 12:16:24 +0100

    Bump.

--- server/setup.py
+++ server/setup.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         'Documentation': 'https://doc.devpi.net',
         'Source Code': 'https://github.com/devpi/devpi'
       },
-      version='6.8.0',
+      version='6.8.1.dev0',
       maintainer="Florian Schulze",
       maintainer_email="mail@pyfidelity.com",
       packages=[
