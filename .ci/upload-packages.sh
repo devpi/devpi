c91ce113eaa4003af742c96106e6563b7e762369
Florian Schulze <mail@florian-schulze.net>
2023-05-23 16:38:23 +0200

    Ignore errors from towncrier during CI builds.
    This was accidentally removed recently.

--- .ci/upload-packages.sh
+++ .ci/upload-packages.sh
@@ -6,27 +6,27 @@ devpi use "${DEVPI_INDEXNAME}"
 devpi push --index root/pypi devpi-server==5.2.0 "${DEVPI_INDEXNAME}"
 devpi push --index root/pypi devpi-server==6.8.0 "${DEVPI_INDEXNAME}"
 pushd common
-towncrier build --yes
+towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd server
-towncrier build --yes
+towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd web
-towncrier build --yes
+towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd client
-towncrier build --yes
+towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd postgresql
-towncrier build --yes
+towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
