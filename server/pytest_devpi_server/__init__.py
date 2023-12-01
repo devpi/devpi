56a65f9bf3bf87aa96c818d401dfc287596eac86
Florian Schulze <mail@florian-schulze.net>
2023-12-01 12:27:58 +0100

    Remove unnecessarily public 'devpiserver_makepypistage' fixture. Refs #780

--- server/pytest_devpi_server/__init__.py
+++ server/pytest_devpi_server/__init__.py
@@ -1,22 +1,4 @@
-from devpi_server.mirror import MirrorStage
-from devpi_server.mirror import MirrorCustomizer
-import pytest
-
-
 def pytest_addoption(parser):
     parser.addoption(
         "--devpi-server-storage-backend", action="store",
         help="run tests with specified dotted name backend")
-
-
-@pytest.fixture
-def devpiserver_makepypistage():
-    def makepypistage(xom):
-        from devpi_server.main import _pypi_ixconfig_default
-        # we copy _pypi_ixconfig_default, otherwise the defaults will
-        # be modified during config updates later on
-        return MirrorStage(
-            xom, username="root", index="pypi",
-            ixconfig=dict(_pypi_ixconfig_default),
-            customizer_cls=MirrorCustomizer)
-    return makepypistage
