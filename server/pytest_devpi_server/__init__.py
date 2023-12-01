379159cf0d916ff12cf90b8a800cb670208eecf3
Florian Schulze <mail@florian-schulze.net>
2023-12-01 12:15:52 +0100

    Remove '--slow' option from exposed pytest_devpi_server plugin to remove clutter if devpi_server is installed globally. Refs #780

--- server/pytest_devpi_server/__init__.py
+++ server/pytest_devpi_server/__init__.py
@@ -5,9 +5,6 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--slow", action="store_true", default=False,
-        help="run slow tests involving remote services (pypi.org)")
-    parser.addoption(
         "--backend", action="store",
         help="run tests with specified dotted name backend")
 
--- server/test_devpi_server/test_views.py
+++ server/test_devpi_server/test_views.py
@@ -1155,7 +1155,7 @@ class TestSubmitValidation:
         assert log2['dst'] == 'user/prod'
         assert log2['src'] == 'user/dev'
 
-    @pytest.mark.skipif("not config.option.slow")
+    @pytest.mark.slow
     def test_last_modified_preserved_on_push(self, submit, testapp, mapp):
         import time
         metadata = {"name": "Pkg5", "version": "2.6", ":action": "submit"}
