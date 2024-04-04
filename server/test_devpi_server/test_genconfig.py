36652c659d627746c733e3b3a76e9d1ae62f6acd
Florian Schulze <mail@florian-schulze.net>
2024-04-04 10:50:35 +0200

    Add test for nginx-devpi-caching.conf.

--- server/test_devpi_server/test_genconfig.py
+++ server/test_devpi_server/test_genconfig.py
@@ -18,3 +18,19 @@ def test_gen_config_all(tmpdir):
     assert b.join("launchd-macos.txt").check()
     assert b.join("net.devpi.plist").check()
     assert b.join("windows-service.txt").check()
+
+
+@pytest.mark.slow
+@pytest.mark.skipif("sys.platform == 'win32'")
+def test_gen_config_caching(tmpdir):
+    tmpdir.chdir()
+    proc = subprocess.Popen(["devpi-gen-config"])  # noqa: S603,S607
+    res = proc.wait()
+    assert res == 0
+    b = tmpdir.join("gen-config")
+    config = b.join("nginx-devpi-caching.conf").read()
+    assert 'proxy_cache_path' in config
+    assert config.count('$bypass_caching') > 1
+    config = b.join("nginx-devpi.conf").read()
+    assert 'proxy_cache_path' not in config
+    assert config.count('$bypass_caching') == 0
