7672465af4e05da2c69ce9b15a1c821643941c09
Florian Schulze <mail@florian-schulze.net>
2025-05-18 10:37:55 +0200

    Install packages before running towncrier to get the correct version.

--- .ci/upload-packages.sh
+++ .ci/upload-packages.sh
@@ -7,26 +7,31 @@ devpi push --index root/pypi devpi-common==3.7.2 "${DEVPI_INDEXNAME}"
 devpi push --index root/pypi devpi-server==5.2.0 "${DEVPI_INDEXNAME}"
 devpi push --index root/pypi devpi-server==6.8.0 "${DEVPI_INDEXNAME}"
 pushd common
+python -m pip install .
 towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd server
+python -m pip install .
 towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd web
+python -m pip install .
 towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd client
+python -m pip install .
 towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
 popd
 pushd postgresql
+python -m pip install .
 towncrier build --yes || true
 head -n 50 CHANGELOG
 devpi upload --no-isolation
