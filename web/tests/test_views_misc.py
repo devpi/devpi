63455b23986ce65a4f1a6b51da409b51e0a4e7ee
Florian Schulze <mail@florian-schulze.net>
2025-06-28 13:50:53 +0200

    Remove backward compatibility code for unsupported devpi-server versions.

--- web/tests/test_views_misc.py
+++ web/tests/test_views_misc.py
@@ -3,14 +3,9 @@ from devpi_common.metadata import parse_version
 from devpi_server import __version__ as _devpi_server_version
 from devpi_web.main import hookimpl
 from functools import partial
+from test_devpi_server.plugin import make_file_url
 import pytest
 import re
-
-
-try:
-    from test_devpi_server.plugin import make_file_url
-except ImportError:
-    from test_devpi_server.conftest import make_file_url  # type: ignore[no-redef]
 
 
 devpi_server_version = parse_version(_devpi_server_version)
