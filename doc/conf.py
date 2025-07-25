397fdaab5ac7c3dbe08c9f21cb82b283bd918d66
Luna Winters <luna@gocreate.fun>
2025-07-25 19:06:23 +0100

    Move documentation to furo theme.

--- doc/conf.py
+++ doc/conf.py
@@ -94,7 +94,7 @@ html_theme_path = ['_themes']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'flask'
+html_theme = 'devpi'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
