4c2ecfaf8c5aad42c2170388c08d8bb6792b4784
Florian Schulze <mail@florian-schulze.net>
2026-02-06 11:43:31 +0100

    Use dependency groups instead of dev-requirements.txt.

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -15,4 +15,4 @@ else
 fi
 
 $UNINSTALL_COMMAND devpi-{common,client,server,web}
-$INSTALL_COMMAND -U -r dev-requirements.txt
+$INSTALL_COMMAND -U --group dev -e common -e client -e postgresql -e server -e web
